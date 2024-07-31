8989c478c40288835a689b4a8fd7093f412746ff
Ken Guan <17guanr@gmail.com>
2024-08-01 00:20:19 +0800

    display likes+comments, do not display views

--- next-app/app/forum/[forumId]/PostStats.jsx
+++ next-app/app/forum/[forumId]/PostStats.jsx
@@ -1,7 +1,38 @@
 import CommentsIcon from "@/public/assets/comments.png";
 import ViewsIcon from "@/public/assets/views.png";
+import LikesIcon from "@/public/assets/likes.png";
+
+const IconAndCount = ({ icon, count }) => {
+    return (
+        <p
+            style={{
+                display: "flex",
+                flexDirection: "row",
+                color: "#A2A2AE",
+                justifyContent: "center",
+                alignItems: "center",
+                marginRight: "10px",
+            }}
+        >
+            <img
+                src={icon}
+                style={{
+                    height: "10px",
+                    marginRight: "5px",
+                }}
+            />
+            {count}
+        </p>
+    );
+};
 
 export const PostStats = ({ post }) => {
+    const views = <IconAndCount icon={ViewsIcon.src} count={post.num_views} />;
+    const comments = (
+        <IconAndCount icon={CommentsIcon.src} count={post.num_comments} />
+    );
+    const likes = <IconAndCount icon={LikesIcon.src} count={post.num_likes} />;
+
     return (
         <div
             style={{
@@ -16,45 +47,9 @@ export const PostStats = ({ post }) => {
                 marginRight: "10px",
             }}
         >
-            <p
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    color: "#A2A2AE",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "10px",
-                }}
-            >
-                <img
-                    src={ViewsIcon.src}
-                    style={{
-                        height: "10px",
-                        marginRight: "5px",
-                    }}
-                />
-                {post.num_views}
-            </p>
-
-            <p
-                style={{
-                    display: "flex",
-                    flexDirection: "row",
-                    color: "#A2A2AE",
-                    justifyContent: "center",
-                    alignItems: "center",
-                    marginRight: "10px",
-                }}
-            >
-                <img
-                    src={CommentsIcon.src}
-                    style={{
-                        height: "10px",
-                        marginRight: "5px",
-                    }}
-                />
-                {post.num_comments}
-            </p>
+            {likes}
+            {comments}
+            {/* {views} */}
         </div>
     );
 };
