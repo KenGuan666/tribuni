89bb78afa77093d4f728d8a37972a030f006c64f
Ken Guan <17guanr@gmail.com>
2024-08-20 22:16:59 -0700

    revise announcement message

--- next-app/app/api/v4/announcement/forumRelease/route.jsx
+++ next-app/app/api/v4/announcement/forumRelease/route.jsx
@@ -1,51 +1,51 @@
 import { getBot } from "@/components/bot";
+import { fetchAllUsersData } from "@/components/db/user";
 
 export async function POST() {
     const bot = getBot();
-    const userId = "ken12358"
-    const chatId = "2114653698"
-
-    const buttons = [[
-        {
-            text: "Subscribe to OP",
-            callback_data: "sub optimism",
-        },
-    ],
-    [
-        {
-            text: "Explore OP Forum",
-            web_app: {
-                url: `${process.env.SERVER_URL}/forum/optimism?username=${userId}&chatid=${chatId}`,
-            }
-        },
-    ]]
-
-    const telegramMessageOptions = {
-        parse_mode: "Markdown",
-        reply_markup: {
-            inline_keyboard: buttons,
-        },
-    };
-
     const forumReleaseText = `
-📣 *Feature Announcement*
-Get up-to-date with Optimism (OP) forum activity in minutes! Try now by tapping "Explore OP Forum" below.
+👀 *Tribuni Feature Announcement*
 
-Tribuni presents you the essence of each forum post, and summarizes community feedback into bullet points. It's truly a must-have for OP forum enthusiasts!
+Tap "Explore OP Forum" for quick, digestible summaries that let you catch up in minutes. Want to stay in the loop without the hassle? Hit "Subscribe to OP" and get a weekly digest straight to your inbox, if you haven't already subscribed.
 
-Subscribe to OP now for a weekly digest message!
+Relax, we’ve got the OP forum covered for you.
 
-Join the alpha user group: [https://t.me/+GQxcYz_80B40ZGMx](https://t.me/+GQxcYz_80B40ZGMx)  
-Give us a like on the OP forum: [link](https://gov.optimism.io/t/tribuni-alpha-launch-telegram-mini-app-built-by-delegates-for-delegates/8568/5?u=sator)  
-Contact us: ask.tribuni@gmail.com
+Join our [alpha group](https://t.me/+GQxcYz_80B40ZGMx) for newest product updates and technical support. 
 `
+    const users = await fetchAllUsersData();
+    for (const user of users) {
+        const userId = user.id;
+        const chatId = user.chatid;
+        const buttons = [[
+            {
+                text: "Subscribe to OP",
+                callback_data: "sub optimism",
+            },
+        ],
+        [
+            {
+                text: "Explore OP Forum",
+                web_app: {
+                    url: `${process.env.SERVER_URL}/forum/optimism?username=${userId}&chatid=${chatId}`,
+                }
+            },
+        ]]
 
-    await bot.sendMessage(
-        chatId,
-        forumReleaseText,
-        telegramMessageOptions
-    )
+        const telegramMessageOptions = {
+            parse_mode: "Markdown",
+            reply_markup: {
+                inline_keyboard: buttons,
+            },
+        };
 
+        if (userId == "ken12358") {
+            await bot.sendMessage(
+                chatId,
+                forumReleaseText,
+                telegramMessageOptions
+            )
+        }
+    }
     return Response.json(
         {
             message: "Success",
