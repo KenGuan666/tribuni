a713c1fa183775d3b148a4cfb0f72e799a48a3c8
Ken Guan <17guanr@gmail.com>
2024-09-02 23:41:20 -0700

    prepare metrics sharing via announcement

---
+++ next-app/app/api/v4/announcement/metrics/route.jsx
@@ -0,0 +1,58 @@
+import { getBot } from "@/components/bot";
+import { fetchAllUsersData } from "@/components/db/user";
+
+export async function POST() {
+    const bot = getBot();
+    const metricsText = `
+❤️ Thank you for choosing Tribuni!
+
+Since our launch, 25 active OP delegates, including you, have chosen Tribuni as their gateway to governance participation. You represent 27.3M OPs in delegated vote power together. That is *30% of total votable OP supply*, *50%+ of votes in recent proposals*, and *healthily above the OP vote quorum*!
+
+We sincerely thank you for your service to the OP community! Kindly help spread the word to other delegates, and send us feature requests or feedback at our [alpha group](https://t.me/+GQxcYz_80B40ZGMx).
+`;
+    const users = await fetchAllUsersData();
+    // const delegates = users.filter(u => u.evm_delegate_addresses)
+    const delegates = users.filter((u) => u.id == "ken12358");
+    for (const user of delegates) {
+        const userId = user.id;
+        const chatId = user.chatid;
+        const buttons = [
+            [
+                {
+                    text: "View All Protocols",
+                    web_app: {
+                        url: `${process.env.SERVER_URL}/protocols?username=${userId}&chatid=${chatId}`,
+                    },
+                },
+            ],
+            [
+                {
+                    text: "Explore OP Forum",
+                    web_app: {
+                        url: `${process.env.SERVER_URL}/forum/optimism?username=${userId}&chatid=${chatId}`,
+                    },
+                },
+            ],
+        ];
+
+        const telegramMessageOptions = {
+            parse_mode: "Markdown",
+            reply_markup: {
+                inline_keyboard: buttons,
+            },
+        };
+
+        try {
+            await bot.sendMessage(chatId, metricsText, telegramMessageOptions);
+        } catch (err) {
+            console.log(`could not announce to ${userId}`);
+            console.log(err);
+        }
+    }
+    return Response.json(
+        {
+            message: "Success",
+        },
+        { status: 201 },
+    );
+}
